5a9adecd4aeb00f6a4d0d3a381fd16effc92106e
Alice Micheloni <alicemicheloni@tutanota.com>
2020-04-23 22:04:38 -0300

    challenge 5 done

--- src/set1/challenge5.rs
+++ src/set1/challenge5.rs
@@ -1,36 +1,43 @@
-fn repeating_xor(input: &[u8], key: String) -> String {
-    let mut ret = String::new();
+fn repeating_xor(input: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut ret = Vec::new();
     let key_len = key.len() - 1;
-    let key: Vec<u8> = key.chars().map(|x| x as u8).collect();
-
-    let mut i = 0;
     let mut key_i = 0;
-    loop {
-        if let Some(s) = input.get(i) {
-            ret.push(hex_to_char(s ^ key[key_i]));
 
-            key_i += 1;
-            i += 1;
-
-            if key_i > key_len {
-                key_i = 0;
-            }
-        } else {
-            break;
+    for h in input.iter() {
+        if key_i > key_len {
+            key_i = 0;
         }
+        ret.push(h ^ key[key_i]);
+        key_i += 1;
+    }
+
+    ret
+}
+
+fn hex_to_char(short: u8) -> char {
+    match short {
+        0x0..=0x9 => (short + '0' as u8) as char,
+        0xa..=0xf => (short - 0xa + 'a' as u8) as char,
+        _ => panic!("hex_to_char only converts short values between 0x0 and 0xf"),
+    }
+}
+
+fn hex_to_string(hex: &[u8]) -> String {
+    let mut ret = String::new();
+    for h in hex.iter() {
+        let hi = (h & 0xF0) >> 4;
+        let lo = h & 0x0F;
+        ret.push_str(&format!("{}{}", hex_to_char(hi), hex_to_char(lo)).to_string());
     }
     ret
 }
 
-fn hex_to_char(input: u8) -> char {
-    (input + '0' as u8) as char
-}
-
 #[test]
 fn test_repeating_xor() {
-    let inp: Vec<u8> = "Burning 'em, if you ain't quick and nimble".chars().map(|x| x as u8).collect();
+    let key = "ICE";
+    let x = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 
-    assert_eq!("".to_string(),
-               repeating_xor(
-                   &inp, "ICE".to_string()));
+    assert_eq!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".to_string(),
+               hex_to_string(&repeating_xor(
+                   x.as_bytes(), key.as_bytes())));
 }
